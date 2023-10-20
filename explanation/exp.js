f5ac128b5e9d5cafa451926a1a1fcbbd8c341af8
nekokujira24wase <120014921+nekokujira24wase@users.noreply.github.com>
2023-10-20 22:57:56 +0900

    exp.jsに説明をコメントアウト

--- explanation/exp.js
+++ explanation/exp.js
@@ -13,13 +13,15 @@ const characters = {
 }
 
 const generatePassword = () => {
-    let staticPassword = "",
-        randomPassword = "",
+    let staticPassword = "", // 使えるcharacterを追加する変数
+        randomPassword = "", // staticPasswordの中からランダムで取り出して、文字を入れる変数
         excludeDuplicate = false,
         passLength = lengthSlider.value;
 
     options.forEach(option => {
         if (option.checked) {
+            // ==は型が違くても値が同じならtrueを返す
+            // ===は型と値が同じじゃないとtrueを返さない
             if (option.id !== "exc-duplicate" && option.id !== "spaces") {
                 staticPassword += characters[option.id];
             } else if (option.id === "spaces") {
@@ -33,6 +35,7 @@ const generatePassword = () => {
     for (let i = 0; i < passLength; i++) {
         let randomChar = staticPassword[Math.floor(Math.random() * staticPassword.length)];
         if (excludeDuplicate) {
+            // 三項演算子
             !randomPassword.includes(randomChar) || randomChar == " " ? randomPassword += randomChar : i--;
         } else {
             randomPassword += randomChar;
@@ -43,26 +46,33 @@ const generatePassword = () => {
 }
 
 const updatePassIndicator = () => {
+    // 三項演算子を入れ子にしている
     passIndicator.id = lengthSlider.value <= 8 ? "weak" : lengthSlider.value <= 16 ? "medium" : "strong";
 }
 
 const updateSlider = () => {
+    // .pass-length spanは.pass-lengthの中のspanタグを指定している
     document.querySelector(".pass-length span").innerText = lengthSlider.value;
     generatePassword();
     updatePassIndicator();
 }
+// 最初にこの関数を呼び出す
 updateSlider();
 
 const copyPassword = () => {
+    // navigator.clipboardはクリップボードへのアクセスを許可するWebAPI
+    // .writeText()はclipboardオブジェクトのメソッドで引数をクリップボードに記録できる
     navigator.clipboard.writeText(passwordInput.value);
-    copyIcon.innerText = "check";
+    copyIcon.innerText = "check"; // テキスト変更する→googleiconが変化する仕組み
     copyIcon.style.color = "#4285f4";
+    // 指定した時間（ミリ秒）後に実行されるsetTimeout関数を定義
     setTimeout(() => {
         copyIcon.innerText = "copy_all";
         copyIcon.style.color = "#707070";
     }, 1500);
 }
 
+// クリック/インプットイベントを設定しイベント発生時に関数が実行されるようにしている
 copyIcon.addEventListener("click", copyPassword);
 lengthSlider.addEventListener("input", updateSlider);
 generateBtn.addEventListener("click", generatePassword);
